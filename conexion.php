1b947c527b9fcd2ed0bde0b8c1bba145ea4cf668
Andoni Vianez <andoni.bartolo@gmail.com>
2020-01-15 21:13:45 +0100

    Corregidos errores tipográficos en el fichero de conexión

--- conexion.php
+++ conexion.php
@@ -6,6 +6,6 @@
 
     $conexion = mysqli_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 
-    $selected = mysqli_select_db($conexion_old, $db) or die("Could not select database");
+    $selected = mysqli_select_db($conexion, $db) or die("Could not select database");
 
 ?>
