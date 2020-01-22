fd3ae37e1aa72b78d3e9010eb87136a30b94e568
Andoni Vianez <andoni.bartolo@gmail.com>
2020-01-22 19:56:58 +0100

    Obtener información de varias tablas, cada uno con su endpoint.

--- api.php
+++ api.php
@@ -10,12 +10,50 @@ echo ("Nos pide los datos de la función...: ".$funcion."<br />");
 // switch para desviar a una u otra función
 if($funcion == 'getUsuarios'){
     getUsuarios($conexion);
+}elseif($funcion == 'getComics'){
+    getComics($conexion);
+}elseif ($funcion == 'getCaracteres') {
+    getCaracteres($conexion);
 }
 
 
 
 function getUsuarios($conexion){
     $sql = "SELECT * FROM usuarios";
+    $sel = mysqli_query($conexion, $sql);
+
+    $data = array();
+	while ($row = mysqli_fetch_assoc($sel)) {
+        //print_r($row);
+		$data[] = $row;
+	}
+
+	$response = array(
+        "data" => $data
+    );
+
+    echo json_encode($response);
+}
+
+function getComics($conexion){
+    $sql = "SELECT * FROM comics";
+    $sel = mysqli_query($conexion, $sql);
+
+    $data = array();
+	while ($row = mysqli_fetch_assoc($sel)) {
+        //print_r($row);
+		$data[] = $row;
+	}
+
+	$response = array(
+        "data" => $data
+    );
+
+    echo json_encode($response);
+}
+
+function getCaracteres($conexion){
+    $sql = "SELECT * FROM caracteres";
     $sel = mysqli_query($conexion, $sql);
 
     $data = array();
